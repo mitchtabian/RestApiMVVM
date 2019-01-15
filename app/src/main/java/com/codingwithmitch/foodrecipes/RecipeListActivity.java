b9cd3b64102743aaaaba006643b96fc098765ae5
Mitch Tabian <mitch@tabian.ca>
2019-01-15 15:20:47 -0800

    building a base activity

--- app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
@@ -1,13 +1,25 @@
 package com.codingwithmitch.foodrecipes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class RecipeListActivity extends AppCompatActivity {
+public class RecipeListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+
+        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mProgressBar.getVisibility() == View.VISIBLE){
+                    showProgressBar(false);
+                }
+                else{
+                    showProgressBar(true);
+                }
+            }
+        });
     }
 }
