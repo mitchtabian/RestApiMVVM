6ca0fd398ecddfcaef1bbf7e95f2552c6457bd69
Mitch Tabian <mitch@tabian.ca>
2019-01-16 09:30:18 -0800

    testing retrofit response objects

--- app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
@@ -1,11 +1,28 @@
 package com.codingwithmitch.foodrecipes;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.codingwithmitch.foodrecipes.requests.RecipeApi;
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeResponse;
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.codingwithmitch.foodrecipes.util.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class RecipeListActivity extends BaseActivity {
+
+    private static final String TAG = "RecipeListActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +32,66 @@ public class RecipeListActivity extends BaseActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mProgressBar.getVisibility() == View.VISIBLE){
-                    showProgressBar(false);
-                }
-                else{
-                    showProgressBar(true);
-                }
+                testRetrofitRequest();
             }
         });
 
-        Retrofit retrofit = ((MyApplication) getApplication()).getRetrofit();
     }
+
+    private void testRetrofitRequest(){
+        Retrofit retrofit = ((MyApplication) getApplication()).getRetrofit();
+        RecipeApi recipeApi = retrofit.create(RecipeApi.class);
+        // Do search using Retrofit
+        Call<RecipeResponse> responseCall = recipeApi
+                .getRecipe(
+                        Constants.API_KEY,
+                        "35382"
+                );
+
+        responseCall.enqueue(new Callback<RecipeResponse>() {
+            @Override
+            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+                Log.d(TAG, "onResponse: Server Response: " + response.toString());
+                if(response.code() == 200){
+                    Recipe recipe = response.body().getRecipe();
+                    Log.d(TAG, "onResponse: " + recipe.toString());
+//                    Log.d(TAG, "onResponse: " + response.body().toString());
+//                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+//                    for(Recipe recipe: recipes){
+//                        Log.d(TAG, "onResponse: " + recipe.toString());
+//                    }
+                }
+                else {
+                    try {
+                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeResponse> call, Throwable t) {
+                Log.d(TAG, "onResponse: ERROR: " + t.getMessage());
+            }
+        });
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
