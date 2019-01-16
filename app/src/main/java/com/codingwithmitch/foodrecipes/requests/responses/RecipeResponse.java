b8873561584b6aad7e48a022ab27344f647da07b
Mitch Tabian <mitch@tabian.ca>
2019-01-16 08:15:19 -0800

    retrofit response objects

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/responses/RecipeResponse.java
@@ -0,0 +1,23 @@
+package com.codingwithmitch.foodrecipes.requests.responses;
+
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RecipeResponse {
+
+    @SerializedName("recipe")
+    @Expose
+    private Recipe recipe;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeResponse{" +
+                "recipe=" + recipe +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/responses/RecipeSearchResponse.java
@@ -0,0 +1,34 @@
+package com.codingwithmitch.foodrecipes.requests.responses;
+
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RecipeSearchResponse {
+
+    @SerializedName("count")
+    @Expose
+    private int count;
+
+    @SerializedName("recipes")
+    @Expose
+    private List<Recipe> recipes;
+
+    public int getCount() {
+        return count;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeSearchResponse{" +
+                "count=" + count +
+                ", recipes=" + recipes +
+                '}';
+    }
+}
