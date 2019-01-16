1096f1340de90ecf7f32f5235f3382c5815cf52a
Mitch Tabian <mitch@tabian.ca>
2019-01-16 08:36:49 -0800

    retrofit interface methods

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/RecipeApi.java
@@ -0,0 +1,27 @@
+package com.codingwithmitch.foodrecipes.requests;
+
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeResponse;
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RecipeApi {
+
+    // SEARCH
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    // GET SPECIFIC RECIPE
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
+
+}
