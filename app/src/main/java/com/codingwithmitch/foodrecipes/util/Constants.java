261f5264880512811ce440d2580f782b3457adc4
Mitch Tabian <mitch@tabian.ca>
2019-01-15 16:01:50 -0800

    rest api introduction

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
@@ -0,0 +1,11 @@
+package com.codingwithmitch.foodrecipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://www.food2fork.com";
+
+    // YOU NEED YOUR OWN API KEY. Get it here: https://www.food2fork.com/about/api
+    public static final String API_KEY = "dadc63b6325aaf398163b40fea9b5e79";
+
+
+}
