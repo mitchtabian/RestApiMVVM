8f0d859e493e31aeac69ac2df44afb534a9450f6
Mitch Tabian <mitch@tabian.ca>
2019-01-17 09:06:20 -0800

    getting started with viewmodels and observables

--- app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
@@ -1,17 +1,20 @@
 package com.codingwithmitch.foodrecipes;
 
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 
 import com.codingwithmitch.foodrecipes.models.Recipe;
 import com.codingwithmitch.foodrecipes.requests.RecipeApi;
 import com.codingwithmitch.foodrecipes.requests.responses.RecipeResponse;
-import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
 import com.codingwithmitch.foodrecipes.util.Constants;
+import com.codingwithmitch.foodrecipes.viewmodels.RecipeListViewModel;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -23,20 +26,29 @@ public class RecipeListActivity extends BaseActivity {
 
     private static final String TAG = "RecipeListActivity";
 
+    private RecipeListViewModel mRecipeListViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
-        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+        mRecipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers(){
+
+        mRecipeListViewModel.getRecipes().observe(this, new Observer<List<Recipe>>() {
             @Override
-            public void onClick(View v) {
-                testRetrofitRequest();
+            public void onChanged(@Nullable List<Recipe> recipes) {
+
             }
         });
-
     }
+
+
 
     private void testRetrofitRequest(){
         Retrofit retrofit = ((MyApplication) getApplication()).getRetrofit();
---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
@@ -0,0 +1,53 @@
+package com.codingwithmitch.foodrecipes.viewmodels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+
+import com.codingwithmitch.foodrecipes.models.Recipe;
+
+import java.util.List;
+
+public class RecipeListViewModel extends AndroidViewModel {
+
+    private MutableLiveData<List<Recipe>> mRecipes = new MutableLiveData<>();
+
+    public RecipeListViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<List<Recipe>> getRecipes() {
+        return mRecipes;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+
+        // 1) make http request using Retrofit to retrieve the data
+        // 2) use a callback method to send the list of recipes back
+        // 3) call mRecipes.setValue(recipes);
+        // 4) Any observers in RecipeListActivity will be automatically updated
+
+        mRecipes.setValue(recipes);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
