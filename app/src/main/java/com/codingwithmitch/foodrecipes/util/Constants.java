2fd10597344abf9e26cc491265486fdfb5500270
Mitch Tabian <mitch@tabian.ca>
2019-01-24 15:00:56 -0800

    network cache test

--- app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
@@ -9,10 +9,6 @@ public class Constants {
 
     // YOU NEED YOUR OWN API KEY. Get it here: https://www.food2fork.com/about/api
     public static final String API_KEY = "dadc63b6325aaf398163b40fea9b5e79";
-//    public static final String API_KEY = "453556cb475252e7e42d65aa11912447";
-//    public static final String API_KEY = "8268001a4008d0695e98d8dd34a8312e";
-//    public static final String API_KEY = "b1b35bb567c88b75c269bc518bb830bb";
-//    public static final String API_KEY = "280d6f829323eaf44207c1e0a13d9493"; // sot
 
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
             {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
