e8f463867313ae52ed9ff0cd51ed0bf3eb2e580d
Mitch Tabian <mitch@tabian.ca>
2019-01-25 14:15:50 -0800

    network security config for http

--- app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
@@ -3,6 +3,7 @@ package com.codingwithmitch.foodrecipes.util;
 public class Constants {
 
     public static final String BASE_URL = "https://www.food2fork.com";
+    public static final int NETWORK_TIMEOUT = 3000;
 
     // YOU NEED YOUR OWN API KEY. Get it here: https://www.food2fork.com/about/api
     public static final String API_KEY = "dadc63b6325aaf398163b40fea9b5e79";
