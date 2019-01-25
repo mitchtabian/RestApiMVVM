cb6028e2d2296875cd6b751b021afea8eb744972
Mitch Tabian <mitch@tabian.ca>
2019-01-25 14:13:20 -0800

    recyclerview setup

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/ServiceGenerator.java
@@ -0,0 +1,42 @@
+package com.codingwithmitch.foodrecipes.requests;
+
+import com.codingwithmitch.foodrecipes.util.Constants;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Modeled after @brittbarak's example on Github
+ * https://github.com/brittBarak/NetworkingDemo
+ * https://twitter.com/brittbarak
+ */
+public class ServiceGenerator {
+
+    private static Retrofit.Builder retrofitBuilder =
+            new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+
+    private static RecipeApi recipeApi = retrofit.create(RecipeApi.class);
+
+    public static RecipeApi getRecipeApi() {
+        return recipeApi;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
