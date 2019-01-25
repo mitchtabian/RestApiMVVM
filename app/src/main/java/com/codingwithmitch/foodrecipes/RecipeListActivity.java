973bee97fb75fb1da4d680f8909cfbffee9c8cd6
Mitch Tabian <mitch@tabian.ca>
2019-01-25 10:05:29 -0800

    testing retrofit response objects

--- app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import com.codingwithmitch.foodrecipes.models.Recipe;
 import com.codingwithmitch.foodrecipes.requests.RecipeApi;
+import com.codingwithmitch.foodrecipes.requests.ServiceGenerator;
 import com.codingwithmitch.foodrecipes.requests.responses.RecipeResponse;
 import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
 import com.codingwithmitch.foodrecipes.util.Constants;
@@ -17,12 +18,11 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
+
 
 public class RecipeListActivity extends BaseActivity {
 
     private static final String TAG = "RecipeListActivity";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,41 +35,61 @@ public class RecipeListActivity extends BaseActivity {
                 testRetrofitRequest();
             }
         });
-
     }
 
     private void testRetrofitRequest(){
-        Retrofit retrofit = ((MyApplication) getApplication()).getRetrofit();
-        RecipeApi recipeApi = retrofit.create(RecipeApi.class);
+        RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
         // Do search using Retrofit
+//        Call<RecipeSearchResponse> responseCall = recipeApi
+//                .searchRecipe(
+//                        Constants.API_KEY,
+//                        "chicken",
+//                        "1"
+//                );
+//
+//        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+//            @Override
+//            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+//                Log.d(TAG, "onResponse: Server Response: " + response.toString());
+//                if(response.code() == 200){
+//                    Log.d(TAG, "onResponse: " + response.body().toString());
+//                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+//                    for(Recipe recipe: recipes){
+//                        Log.d(TAG, "onResponse: " + recipe.toString());
+//                    }
+//                }
+//                else {
+//                    try {
+//                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+
+        // do get using retrofit
         Call<RecipeResponse> responseCall = recipeApi
                 .getRecipe(
                         Constants.API_KEY,
                         "35382"
                 );
 
-        responseCall.enqueue(new Callback<RecipeResponse>() {
-            @Override
-            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
-                Log.d(TAG, "onResponse: Server Response: " + response.toString());
-                if(response.code() == 200){
-                    Recipe recipe = response.body().getRecipe();
-                    Log.d(TAG, "onResponse: " + recipe.toString());
-//                    Log.d(TAG, "onResponse: " + response.body().toString());
-//                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
-//                    for(Recipe recipe: recipes){
-//                        Log.d(TAG, "onResponse: " + recipe.toString());
-//                    }
-                }
-                else {
-                    try {
-                        Log.d(TAG, "onResponse: " + response.errorBody().string());
-                    } catch (IOException e) {
-                        e.printStackTrace();
+       responseCall.enqueue(new Callback<RecipeResponse>() {
+                @Override
+                public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+                    Log.d(TAG, "onResponse: Server Response: " + response.toString());
+                    if (response.code() == 200) {
+                        Recipe recipe = response.body().getRecipe();
+                        Log.d(TAG, "onResponse: " + recipe.toString());
+                    }
+                    else{
+                        try {
+                            Log.d(TAG, "onResponse: " + response.errorBody().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-            }
-
             @Override
             public void onFailure(Call<RecipeResponse> call, Throwable t) {
                 Log.d(TAG, "onResponse: ERROR: " + t.getMessage());
@@ -78,20 +98,3 @@ public class RecipeListActivity extends BaseActivity {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
