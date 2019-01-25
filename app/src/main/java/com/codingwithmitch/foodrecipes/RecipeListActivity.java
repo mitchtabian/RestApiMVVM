77e5396069ee51be677a2ac048c321c5a71313c6
Mitch Tabian <mitch@tabian.ca>
2019-01-25 14:21:01 -0800

    themes colors and toolbars

--- app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
@@ -1,29 +1,33 @@
 package com.codingwithmitch.foodrecipes;
 
-
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
 
 
+import com.codingwithmitch.foodrecipes.adapters.OnRecipeListener;
 import com.codingwithmitch.foodrecipes.adapters.RecipeRecyclerAdapter;
 import com.codingwithmitch.foodrecipes.models.Recipe;
+
+import com.codingwithmitch.foodrecipes.util.Testing;
 import com.codingwithmitch.foodrecipes.viewmodels.RecipeListViewModel;
+
 
 import java.util.List;
 
 
-public class RecipeListActivity extends BaseActivity implements RecipeRecyclerAdapter.OnRecipeListener {
+
+public class RecipeListActivity extends BaseActivity implements OnRecipeListener {
 
     private static final String TAG = "RecipeListActivity";
 
     private RecipeListViewModel mRecipeListViewModel;
-
     private RecyclerView mRecyclerView;
     private RecipeRecyclerAdapter mAdapter;
 
@@ -37,7 +41,24 @@ public class RecipeListActivity extends BaseActivity implements RecipeRecyclerAd
 
         initRecyclerView();
         subscribeObservers();
-        mRecipeListViewModel.search("barbeque", 1);
+        testRetrofitRequest();
+    }
+
+    private void subscribeObservers(){
+
+        mRecipeListViewModel.getRecipes().observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(@Nullable List<Recipe> recipes) {
+                if(recipes != null){
+                    Testing.printRecipes("network test", recipes);
+                }
+                mAdapter.setRecipes(recipes);
+            }
+        });
+    }
+
+    private void testRetrofitRequest(){
+        mRecipeListViewModel.searchRecipesApi("chicken", 1);
     }
 
     private void initRecyclerView(){
@@ -46,29 +67,16 @@ public class RecipeListActivity extends BaseActivity implements RecipeRecyclerAd
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void subscribeObservers(){
-
-        mRecipeListViewModel.getRecipes().observe(this, new Observer<List<Recipe>>() {
-            @Override
-            public void onChanged(@Nullable List<Recipe> recipes) {
-                Log.d(TAG, "onChanged: updating list with new recipes. Num recipes: " + recipes.size());
-                mAdapter.setRecipes(recipes);
-            }
-        });
-    }
-
-
     @Override
     public void onRecipeClick(int position) {
-        Log.d(TAG, "onRecipeClick: clicked a recipe at position: " + position);
+        Log.d(TAG, "onRecipeClick: clicked. " + position);
+    }
+
+    @Override
+    public void onCategoryClick(String category) {
+
     }
 }
-
-
-
-
-
-
 
 
 
