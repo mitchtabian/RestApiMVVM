d0474046b640d3e1043f7802b595b4378c327305
Mitch Tabian <mitch@tabian.ca>
2019-01-25 11:11:48 -0800

    getting started with viewmodels and observables

--- app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
@@ -1,7 +1,7 @@
 package com.codingwithmitch.foodrecipes.viewmodels;
 
 import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
@@ -10,12 +10,12 @@ import com.codingwithmitch.foodrecipes.models.Recipe;
 
 import java.util.List;
 
-public class RecipeListViewModel extends AndroidViewModel {
+public class RecipeListViewModel extends ViewModel {
 
     private MutableLiveData<List<Recipe>> mRecipes = new MutableLiveData<>();
 
-    public RecipeListViewModel(@NonNull Application application) {
-        super(application);
+    public RecipeListViewModel() {
+        
     }
 
     public LiveData<List<Recipe>> getRecipes() {
