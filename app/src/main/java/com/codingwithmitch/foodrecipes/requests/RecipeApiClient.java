6d988bd78cd1551cd4f80a91d3fd8c6038039f87
Mitch Tabian <mitch@tabian.ca>
2019-01-25 11:39:52 -0800

    executors for background tasks

--- app/src/main/java/com/codingwithmitch/foodrecipes/requests/RecipeApiClient.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/RecipeApiClient.java
@@ -3,9 +3,15 @@ package com.codingwithmitch.foodrecipes.requests;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 
+import com.codingwithmitch.foodrecipes.AppExecutors;
 import com.codingwithmitch.foodrecipes.models.Recipe;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static com.codingwithmitch.foodrecipes.util.Constants.NETWORK_TIMEOUT;
+
 
 public class RecipeApiClient {
 
@@ -25,6 +31,26 @@ public class RecipeApiClient {
 
     public LiveData<List<Recipe>> getRecipes(){
         return mRecipes;
+    }
+
+    public void searchRecipesApi(){
+        final Future handler = AppExecutors.get().networkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+
+                // retrieve data from rest api
+//                mRecipes.setValue(dataFromRestApi);
+            }
+        });
+
+        // Set a timeout for the data refresh
+        AppExecutors.get().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                // let the user know it timed out
+                handler.cancel(true);
+            }
+        }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
     }
 }
 
