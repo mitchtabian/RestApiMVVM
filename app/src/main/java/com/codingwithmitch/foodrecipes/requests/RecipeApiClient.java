5367361df4e1b2e2bc48ae7e97bd83ed9a54bb55
Mitch Tabian <mitch@tabian.ca>
2019-01-28 13:41:52 -0800

    recyclerview with multiple view types part 2

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/RecipeApiClient.java
@@ -0,0 +1,140 @@
+package com.codingwithmitch.foodrecipes.requests;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+
+import com.codingwithmitch.foodrecipes.AppExecutors;
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.codingwithmitch.foodrecipes.util.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+import static com.codingwithmitch.foodrecipes.util.Constants.NETWORK_TIMEOUT;
+
+
+public class RecipeApiClient {
+
+    private static final String TAG = "RecipeApiClient";
+
+    private static RecipeApiClient instance;
+    private MutableLiveData<List<Recipe>> mRecipes;
+    private RetrieveRecipesRunnable mRetrieveRecipesRunnable;
+
+
+    public static RecipeApiClient getInstance(){
+        if(instance == null){
+            instance = new RecipeApiClient();
+        }
+        return instance;
+    }
+
+    private RecipeApiClient() {
+        mRecipes = new MutableLiveData<>();
+    }
+
+    public LiveData<List<Recipe>> getRecipes(){
+        return mRecipes;
+    }
+
+    public void searchRecipesApi(String query, int pageNumber){
+        if(mRetrieveRecipesRunnable != null){
+            mRetrieveRecipesRunnable = null;
+        }
+        mRetrieveRecipesRunnable = new RetrieveRecipesRunnable(query, pageNumber);
+        final Future handler = AppExecutors.get().networkIO().submit(mRetrieveRecipesRunnable);
+
+        // Set a timeout for the data refresh
+        AppExecutors.get().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                // let the user know it timed out
+                handler.cancel(true);
+            }
+        }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    private class RetrieveRecipesRunnable implements Runnable{
+
+        private String query;
+        private int pageNumber;
+        private boolean cancelRequest;
+
+        private RetrieveRecipesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                Response response = getRecipes(query, pageNumber).execute();
+                if(cancelRequest){
+                    return;
+                }
+                if(response.code() == 200){
+                    List<Recipe> list = new ArrayList<>(((RecipeSearchResponse)response.body()).getRecipes());
+                    if(pageNumber == 1){
+                        mRecipes.postValue(list);
+                    }
+                    else{
+                        List<Recipe> currentRecipes = mRecipes.getValue();
+                        currentRecipes.addAll(list);
+                        mRecipes.postValue(currentRecipes);
+                    }
+                }
+                else{
+                    String error = response.errorBody().string();
+                    Log.e(TAG, "run: error: " + error);
+                    mRecipes.postValue(null);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                mRecipes.postValue(null);
+            }
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query, int pageNumber){
+            return ServiceGenerator.getRecipeApi().searchRecipe(
+                    Constants.API_KEY,
+                    query,
+                    String.valueOf(pageNumber));
+        }
+
+        private void cancelRequest(){
+            Log.d(TAG, "cancelRequest: canceling the retrieval query");
+            cancelRequest = true;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
