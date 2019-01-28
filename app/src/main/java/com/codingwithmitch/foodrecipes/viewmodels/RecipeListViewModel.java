37eea6e68af5a815e1ce244c45e7b377238f6a45
Mitch Tabian <mitch@tabian.ca>
2019-01-28 13:51:59 -0800

    custom back navigation

--- app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
@@ -1,80 +1,45 @@
 package com.codingwithmitch.foodrecipes.viewmodels;
 
-import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
-import android.support.annotation.NonNull;
+import android.arch.lifecycle.ViewModel;
 
 import com.codingwithmitch.foodrecipes.models.Recipe;
-import com.codingwithmitch.foodrecipes.repositories.RecipeListCallback;
 import com.codingwithmitch.foodrecipes.repositories.RecipeRepository;
-import com.codingwithmitch.foodrecipes.util.Constants;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class RecipeListViewModel extends AndroidViewModel implements
-        RecipeListCallback
-{
+public class RecipeListViewModel extends ViewModel {
 
     private RecipeRepository mRecipeRepository;
-    private MutableLiveData<List<Recipe>> mRecipes = new MutableLiveData<>();
     private boolean mIsViewingRecipes;
 
-    public RecipeListViewModel(@NonNull Application application) {
-        super(application);
-        mRecipeRepository = RecipeRepository.getInstance(application);
-        mRecipeRepository.setRecipeListCallback(this);
+    public RecipeListViewModel() {
+        mRecipeRepository = RecipeRepository.getInstance();
     }
 
     public LiveData<List<Recipe>> getRecipes() {
-        return mRecipes;
+        return mRecipeRepository.getRecipes();
     }
 
-    public boolean getIsViewingRecipes() {
+    public void searchRecipesApi(String query, int pageNumber){
+        mIsViewingRecipes = true;
+        mRecipeRepository.searchRecipesApi(query, pageNumber);
+    }
+
+    public boolean isViewingRecipes() {
         return mIsViewingRecipes;
     }
 
-    public void setIsViewingRecipes(boolean isViewingRecipes) {
-        this.mIsViewingRecipes = isViewingRecipes;
+    public void setIsViewingRecipes(boolean isViewingRecipes){
+        mIsViewingRecipes = isViewingRecipes;
     }
 
-    @Override
-    public void setRecipes(List<Recipe> recipes) {
-
-        // 1) make http request using Retrofit to retrieve the data
-        // 2) use a callback method to send the list of recipes back
-        // 3) call mRecipes.setValue(recipes);
-        // 4) Any observers in RecipeListActivity will be automatically updated
-
-        mRecipes.setValue(recipes);
-    }
-
-    public void displaySearchCategories(){
-        mIsViewingRecipes = false;
-        List<Recipe> categories = new ArrayList<>();
-        for(int i = 0; i < Constants.DEFAULT_SEARCH_CATEGORIES.length; i++){
-            Recipe recipe = new Recipe();
-            recipe.setTitle(Constants.DEFAULT_SEARCH_CATEGORIES[i]);
-            recipe.setImage_url(Constants.DEFAULT_SEARCH_CATEGORY_IMAGES[i]);
-            recipe.setSocial_rank(-1);
-            categories.add(recipe);
+    public boolean onBackPressed(){
+        if(mIsViewingRecipes){
+            mIsViewingRecipes = false;
+            return false;
         }
-        mRecipes.setValue(categories);
-    }
-
-    private void displayLoadingScreen(){
-        Recipe recipe = new Recipe();
-        recipe.setTitle("LOADING...");
-        List<Recipe> loadingList = new ArrayList<>();
-        loadingList.add(recipe);
-        mRecipes.setValue(loadingList);
-    }
-
-    public void search(String query, int pageNumber){
-        displayLoadingScreen();
-        mRecipeRepository.searchApi(query, pageNumber);
+        return true;
     }
 }
 
