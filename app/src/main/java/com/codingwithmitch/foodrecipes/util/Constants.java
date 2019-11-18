18d0ca15e989b361e3b29b3b894776c2c0d4d2f8
Mitch Tabian <mitch@tabian.ca>
2019-11-18 08:35:38 -0800

    Food2Fork has been shutdown. Data has been copied to a heroku app.

--- app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
@@ -2,12 +2,14 @@ package com.codingwithmitch.foodrecipes.util;
 
 public class Constants {
 
-    public static final String BASE_URL = "https://www.food2fork.com";
+    //    public static final String BASE_URL = "https://www.food2fork.com";
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+
+    // API_KEY is no longer necessary since food2fork has shutdown. This can be empty it doesn't matter.
+//    public static final String API_KEY = "453556cb475252e7e42d65aa11912447";
+    public static final String API_KEY = "";
+
     public static final int NETWORK_TIMEOUT = 3000;
-
-    // YOU NEED YOUR OWN API KEY!!!!!!!!!!!!! https://www.food2fork.com/about/api
-    public static final String API_KEY = "dadc63b6325aaf398163b40fea9b5e79";
-
 
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
             {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
